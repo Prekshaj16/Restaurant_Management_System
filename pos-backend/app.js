bfd7ae802c9da7d467c837f0d3972e7ca4245867
Prekshaj16 <prekshajainsurat@gmail.com>
2026-05-17 18:40:42 +0530

    upadted app.js

--- pos-backend/app.js
+++ pos-backend/app.js
@@ -4,25 +4,29 @@ const config = require("./config/config");
 const globalErrorHandler = require("./middlewares/globalErrorHandler");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
+
 const app = express();
 
-
 const PORT = config.port;
+
 connectDB();
 
 // Middlewares
 app.use(cors({
     credentials: true,
-    origin: ['https://restaurantfrontend-ten.vercel.app']
-}))
-app.use(express.json()); // parse incoming request in json format
-app.use(cookieParser())
+    origin: [
+        "http://localhost:5173",
+        "https://restaurantfrontend-ten.vercel.app"
+    ]
+}));
 
+app.use(express.json());
+app.use(cookieParser());
 
 // Root Endpoint
-app.get("/", (req,res) => {
-    res.json({message : "Hello from POS Server!"});
-})
+app.get("/", (req, res) => {
+    res.json({ message: "Hello from POS Server!" });
+});
 
 // Other Endpoints
 app.use("/api/user", require("./routes/userRoute"));
@@ -33,8 +37,7 @@ app.use("/api/payment", require("./routes/paymentRoute"));
 // Global Error Handler
 app.use(globalErrorHandler);
 
-
 // Server
 app.listen(PORT, () => {
-    console.log(`☑️  POS Server is listening on port ${PORT}`);
-})
+    console.log(`☑️ POS Server is listening on port ${PORT}`);
+});
